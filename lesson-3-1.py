b5c15fed7cf7b819201618e4da61d5cbdd4f46be
Igor S <kontarkovi@yandex.com>
2021-03-12 15:41:07 +0500

    Выполнил задание 1. Корректировка

--- lesson-3-1.py
+++ lesson-3-1.py
@@ -5,10 +5,10 @@
 """
 
 
-def divide2dig(x, y):
-    'divide2dig() - возвращает результат деления двух чисел'
+def divide2dig(xx, yy):
+    """divide2dig() - возвращает результат деления двух чисел"""
     try:
-        return x / y
+        return xx / yy
     except ZeroDivisionError:
         print("ОШИБКА! Деление на ноль!")
     except ValueError:
@@ -17,12 +17,11 @@ def divide2dig(x, y):
         print("ОШИБКА! Не является числом!")
 
 
-
 try:
     digits = [float(input(f'Введите число {"X" if ind == 0 else "Y"} : ')) for ind in range(0, 2)]
     # print(digits)
     x, y = digits[0], digits[1]
     print(f"ДЕЛЕНИЕ: {x} / {y} = {divide2dig(x, y)}")
-    print('--',divide2dig.__doc__)
+    print('--', divide2dig.__doc__)
 except ValueError:
-        print("ОШИБКА! Не является числом!")
+    print("ОШИБКА! Не является числом!")
